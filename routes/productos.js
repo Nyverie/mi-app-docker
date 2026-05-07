1b38ed2c076d5f30bdff65e5c480fb625abf23d0
Faty <fachima.cr@gmail.com>
2026-05-07 12:10:47 -0500

    Add products route with MySQL connection

---
+++ routes/productos.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: 'mysql-db',
+  user: 'root',
+  password: 'root123',
+  database: 'miapp'
+});
+
+db.connect();
+
+router.get('/', (req, res) => {
+  db.query('SELECT * FROM productos', (err, results) => {
+    if (err) return res.status(500).json({ error: err.message });
+    res.json(results);
+  });
+});
+
+module.exports = router;
